7356f67aba3cee79e07dcc97db33b4ff85d6ad79
Site Galleria <rahulk@sitegalleria.com>
2020-10-01 10:51:50 +0530

    Commit

--- src/modules/Socket.js
+++ src/modules/Socket.js
@@ -10,7 +10,6 @@ export default class Socket {
         this.app = app;
 
         this.clients = [];
-        this.client_count = 0;
         this.execute();
         _this = this;
 
@@ -21,25 +20,28 @@ export default class Socket {
 
         this.io.of('/admin').on('connect', (socket) => {
 
-            console.log(`Connected - ${socket.id}`)
-            console.log(this.client_count)
-            this.clients.push(socket);
-            this.client_count++;
+            var handshakeData = socket.request;
+            let data = { 'socket_id' : socket.id , 'user_id' : handshakeData._query['user_id'] ,"status" : 'online' }
+
+            let index = _this.clients.findIndex(x => x.user_id == data.user_id);
+            index == -1 ? _this.clients.push(data) : _this.clients.splice(index,1,data);
+
+            console.log(`Connected : ${socket.id}`)
 
             socket.on('online', () => {
+                _this.respondAllSockets({"Client_Online": _this.clients.filter(x => x.status == 'online').length , "Client_Offline" : _this.clients.filter(x => x.status == 'offline').length , "Clients" : _this.clients });
+            })
 
-                _this.respondAllSockets({"Client_Count":_this.client_count})
+            socket.on('clear-counter',() => {
+                _this.clients = [];
+                this.io.of('admin').emit('close-window',{});
             })
 
             socket.on('disconnect', () => {
-
-                let index = _this.clients.findIndex(x => x.id == socket.id);
-                _this.clients.splice(index,1)
-                _this.client_count--
-                console.log(_this.client_count)
-                console.log(`Disconnected - ${socket.id}`)
+                let index = _this.clients.findIndex(x => x.socket_id == socket.id);
+                index > -1 ? _this.clients[index]['status'] = 'offline' : {}
+                console.log(`Disconnected - ${socket.id}`);
             });
-
         
         })
     }
